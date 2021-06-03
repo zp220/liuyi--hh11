5744219984af4f21911e03507d0133636590403b
zp220 <1730300297@qq.com>
2021-06-03 15:30:12 +0800

    排序

--- xiaozhou/inxter.js
+++ xiaozhou/inxter.js
@@ -1,6 +1,6 @@
 
-const quickSort = require("./baolu");
-const sub = require("./sub");
+const quickSort = require("./baolu.js");
 const arr1 = [1,4,3,6,4,2];
 const newArr = quickSort(arr1);
 console.log(newArr);
+console.log(quickSort)
